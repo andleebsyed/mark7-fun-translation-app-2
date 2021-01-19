9eaf754b259f9127be1b423528d4f7805dc8efc9
Syed Andleeb <andleebsyed75@gmail.com>
2021-01-19 21:02:41 +0530

    update : added js file

---
+++ app.js
@@ -0,0 +1,22 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#text-input")
+var textOutput = document.querySelector("#text-output")
+var serverUrl  = "https://api.funtranslations.com/translate/pirate.json"
+
+function urlGenerator(input){
+    return serverUrl + "?" + "text=" + input 
+}
+
+function errorHandler(error){
+    alert("There is something wrong with the server!Please try again later")
+}
+
+function ClickHandler(){ 
+    var inputText = textInput.value
+    fetch(urlGenerator(inputText))
+    .then(response => response.json())
+    .then (json => textOutput.innerText = json.contents.translated )
+    .catch(errorHandler);
+}
+
+btnTranslate.addEventListener("click",ClickHandler)
